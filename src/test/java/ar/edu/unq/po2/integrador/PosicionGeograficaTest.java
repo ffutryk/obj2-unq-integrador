a4a0d3355ea0128b20d0c770d2ea49977748cd06
Uriel Benitez <unbenitez@gmail.com>
2025-11-02 02:54:42 -0300

    feat: agregar PosicionGeografica + test de unidad

---
+++ src/test/java/ar/edu/unq/po2/integrador/PosicionGeograficaTest.java
@@ -0,0 +1,17 @@
+package ar.edu.unq.po2.integrador;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+
+class PosicionGeograficaTest {
+
+	@Test
+	void testUnaPosicionGeograficaSabeResponderAQueDistanciaSeEncuentraDeOtraPosicionGeografica() {
+		PosicionGeografica puertoBsAs = new PosicionGeografica(34d, 58d);
+		PosicionGeografica puertoRosario = new PosicionGeografica(32d, 60d);
+		assertEquals(300d, puertoBsAs.distanciaHasta(puertoRosario), 10d);
+	}
+
+}
