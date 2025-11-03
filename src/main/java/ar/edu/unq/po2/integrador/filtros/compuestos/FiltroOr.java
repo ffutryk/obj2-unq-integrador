1037aaae3e97ce684e8ace27ff11e3a74fa57689
Fabricio Futryk <ffutryk52@gmail.com>
2025-11-03 00:52:01 -0300

    feat: implementar filtros compuestos FiltroAnd y FiltroOr con tests

---
+++ src/main/java/ar/edu/unq/po2/integrador/filtros/compuestos/FiltroOr.java
@@ -0,0 +1,15 @@
+package ar.edu.unq.po2.integrador.filtros.compuestos;
+
+import ar.edu.unq.po2.integrador.Viaje;
+import ar.edu.unq.po2.integrador.filtros.IFiltroViaje;
+
+public class FiltroOr extends FiltroCompuesto {
+	public FiltroOr(IFiltroViaje izquierda, IFiltroViaje derecha) {
+        super(izquierda, derecha);
+    }
+    
+    @Override
+    public boolean cumpleFiltro(Viaje viaje) {
+        return izquierda.cumpleFiltro(viaje) || derecha.cumpleFiltro(viaje);
+    }
+}
---
+++ src/test/java/ar/edu/unq/po2/integrador/filtros/compuestos/FiltroAndTest.java
@@ -0,0 +1,59 @@
+package ar.edu.unq.po2.integrador.filtros.compuestos;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import ar.edu.unq.po2.integrador.Viaje;
+import ar.edu.unq.po2.integrador.filtros.IFiltroViaje;
+import ar.edu.unq.po2.integrador.filtros.compuestos.FiltroAnd;
+
+class FiltroAndTest {
+
+	FiltroAnd filtroAnd;
+	IFiltroViaje filtroIzquierda;
+	IFiltroViaje filtroDerecha;
+	Viaje unViaje;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		filtroIzquierda = mock(IFiltroViaje.class);
+		filtroDerecha = mock(IFiltroViaje.class);
+		filtroAnd = new FiltroAnd(filtroIzquierda, filtroDerecha);
+		unViaje = mock(Viaje.class);
+	}
+	
+	@Test
+	void testUnFiltroAndCumpleCuandoAmbosFiltrosCumplen() {
+		when(filtroIzquierda.cumpleFiltro(unViaje)).thenReturn(true);
+		when(filtroDerecha.cumpleFiltro(unViaje)).thenReturn(true);
+		
+		assertTrue(filtroAnd.cumpleFiltro(unViaje));
+	}
+	
+	@Test
+	void testUnFiltroAndNoCumpleCuandoElFiltroIzquierdoNoCumple() {
+		when(filtroIzquierda.cumpleFiltro(unViaje)).thenReturn(false);
+		when(filtroDerecha.cumpleFiltro(unViaje)).thenReturn(true);
+		
+		assertFalse(filtroAnd.cumpleFiltro(unViaje));
+	}
+	
+	@Test
+	void testUnFiltroAndNoCumpleCuandoElFiltroDerechoNoCumple() {
+		when(filtroIzquierda.cumpleFiltro(unViaje)).thenReturn(true);
+		when(filtroDerecha.cumpleFiltro(unViaje)).thenReturn(false);
+		
+		assertFalse(filtroAnd.cumpleFiltro(unViaje));
+	}
+	
+	@Test
+	void testUnFiltroAndNoCumpleCuandoNingunoDeLosFiltrosCumple() {
+		when(filtroIzquierda.cumpleFiltro(unViaje)).thenReturn(false);
+		when(filtroDerecha.cumpleFiltro(unViaje)).thenReturn(false);
+		
+		assertFalse(filtroAnd.cumpleFiltro(unViaje));
+	}
+}
